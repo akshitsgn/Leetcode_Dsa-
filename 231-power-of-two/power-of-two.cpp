8a44ac6e96520bc82cc52060772768ef0f2f2847
Akshit Thakur <138555638+akshitsgn@users.noreply.github.com>
2025-07-09 14:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.64%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+      if(n>0){
+      if((n & (n - 1)) == 0){
+        return true;
+      }
+      else{
+        return false;
+      }
+      }
+      
+      return false;
+    }
+};
