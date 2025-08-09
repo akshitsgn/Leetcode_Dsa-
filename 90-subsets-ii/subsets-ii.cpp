46342a0f9b600679a505f814932f70bb4e0d7ed3
Akshit Thakur <138555638+akshitsgn@users.noreply.github.com>
2025-08-09 23:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (66.83%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    void sumret(vector<int> &arr, vector<int> &ans1, set<vector<int>> &ans, int sum, int ind) {
-        if (ind == arr.size()) {
-            ans.insert(ans1);
-            return;
-        }
-        ans1.push_back(arr[ind]);
-        sumret(arr, ans1, ans, sum, ind + 1);
+void sumret(vector<int>&arr ,vector<int>&ans1, vector<vector<int>>&ans,int ind){
+      ans.push_back(ans1);
+      for(int i=ind;i<arr.size();i++){
+        if(i!=ind && arr[i]==arr[i-1]){continue;}
+        ans1.push_back(arr[i]);
+        sumret(arr,ans1,ans,i+1);
         ans1.pop_back();
-        sumret(arr, ans1, ans, sum, ind + 1);
-    }
-
+      }
+  }
     vector<vector<int>> subsetsWithDup(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        set<vector<int>> ans;
-        vector<int> ans1;
-        int sum = 0;
-        sumret(arr, ans1, ans, sum, 0);
-        return vector<vector<int>>(ans.begin(), ans.end());
+         vector<vector<int>>ans;
+         vector<int>ans1;
+         sort(arr.begin(),arr.end());
+         sumret(arr,ans1,ans,0);
+         return ans;
     }
 };
