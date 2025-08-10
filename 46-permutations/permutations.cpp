3753a7b18f0d82c03821adaaeef6e866b96a3428
Akshit Thakur <138555638+akshitsgn@users.noreply.github.com>
2025-08-10 14:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (86.86%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    void permu(vector<int>&nums , vector<int>&ans , vector<vector<int>>&ans1 , vector<vector<int>>&ans2 , int ind){
+    void permu(vector<int>&nums , vector<int>&ans , vector<vector<int>>&ans1 , int ind){
         if(ind>=nums.size()){
             ans1.push_back(ans);
             return;
         }
-        vector<int>check = ans2[ind];
-        for(int i=0;i<check.size();i++){
+        for(int i=0;i<nums.size();i++){
             int ch =0;
             for(int j=0;j<ans.size();j++){
-                if(check[i]==ans[j]){ch++;break;}
+                if(nums[i]==ans[j]){ch++;break;}
             }
            if(ch==1){continue;}
-           ans.push_back(check[i]);
-           permu(nums,ans,ans1,ans2,ind+1);
+           ans.push_back(nums[i]);
+           permu(nums,ans,ans1,ind+1);
            ans.pop_back();
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans1;
-        vector<vector<int>>ans2;
         vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            ans2.push_back(nums);
-        }
-        permu(nums,ans,ans1,ans2,0);
+        permu(nums,ans,ans1,0);
         return ans1;
     }
 };
